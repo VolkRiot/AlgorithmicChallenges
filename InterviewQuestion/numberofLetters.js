0d5a5fa966d5eaac7a15a751733766e75ecf9387
Mikhail Metrikin <metrikin@gmail.com>
2017-07-11 17:41:56 -0700

    Another algo solution

---
+++ InterviewQuestion/numberofLetters.js
@@ -0,0 +1,36 @@
+/*Option 2*
+
+Write a function that counts how many times each letter in a string occurs.
+
+For example:
+
+```var letter_counts = count_letters("Example sentence");
+
+console.log(letter_counts['e']); // 5
+```
+
+Your function should be case-insensitive. In other words, ‘E’ and ‘e’ should
+both increment the count for the letter ‘e’. */
+
+function count_letters(string) {
+
+  let count = {};
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let lowerString = String(string).toLowerCase();
+
+  alphabet.split('').forEach(alpha => {
+    count[alpha] = 0;
+  });
+
+  lowerString.split('').forEach(letter => {
+    if(/[a-z]/.test(letter)) {
+      count[letter]++;
+    }
+  });
+
+  return count;
+}
+
+var letter_counts = count_letters("Example sentence");
+
+console.log(letter_counts['e']); // 5
