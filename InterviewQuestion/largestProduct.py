1f62f3245044b67db06837138290b86183d1f45d
Mikhail Metrikin <metrikin@gmail.com>
2017-08-01 19:53:14 -0700

    Another conversion to an array

--- InterviewQuestion/largestProduct.py
+++ InterviewQuestion/largestProduct.py
@@ -23,4 +23,62 @@ def largest_multiple(arr):
 
     return max_product;
 
-print(largest_multiple([-100, 2, 42, 100]))
+# Smart O(n)
+
+def largest_multiple_smarter(arr):
+
+    largestPos = None
+    lrgPosIndex = None
+    secondPos = None
+
+    largestNeg = None
+    lrgNegIndx = None
+    secondNeg = None
+
+    maxProductPos = None
+    maxProductNeg = None
+
+    for i, val in enumerate(arr):
+        if val >= 0:
+            if not largestPos:
+                largestPos = val
+                lrgPosIndex = i
+
+            elif val > largestPos:
+                secondPos = largestPos
+                largestPos = val
+                lrgPosIndex = i
+
+            elif (not secondPos or val > secondPos) and i != lrgPosIndex:
+                secondPos = each
+
+        else:
+
+            if not largestNeg:
+                largestNeg = val
+                lrgNegIndx = i
+
+            elif val < largestNeg:
+                secondNeg = largestNeg
+                largestNeg = val
+                lrgNegIndx = i
+
+            elif ((not secondNeg or (val < secondNeg)) and i != lrgNegIndx):
+                secondNeg = each
+
+
+    if largestPos and secondPos:
+        maxProductPos = largestPos * secondPos
+
+    if largestNeg and secondNeg:
+        maxProductNeg = largestNeg * secondNeg
+
+    if maxProductNeg and maxProductPos:
+        return max(maxProductPos, maxProductNeg)
+    elif maxProductPos:
+        return maxProductPos
+    else:
+        return maxProductNeg
+
+
+print(largest_multiple_smarter([-100, 2, 42, 100]))
