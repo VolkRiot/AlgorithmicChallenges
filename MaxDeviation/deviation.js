f9d2738bcbf717e97ca7806ba6acfa7cc2f150da
Mikhail Metrikin <metrikin@gmail.com>
2017-09-23 16:29:43 -0700

    WIP

--- MaxDeviation/deviation.js
+++ MaxDeviation/deviation.js
@@ -29,12 +29,85 @@ function deviation(arr, dev) {
   return maxDiff;
 }
 
-let randArr = shuffledArr(100000);
+function deviationBetter(arr, dev) {
+  let queue = [];
+  let smallest = Number.POSITIVE_INFINITY;
+  let smlIndex = undefined;
+  let largest = Number.NEGATIVE_INFINITY;
+  let lrgIndex = undefined;
+  let maxDiff = Number.NEGATIVE_INFINITY;
 
-let start = performance();
-console.log(deviation(randArr, 1000));
-let end = performance();
+  for (let i = 0; i < dev; i++) {
+    let val = arr[i];
 
-console.log(
-  `Algo for deviation finished in ${((end - start) / 1000).toFixed(3)} seconds`
-);
+    if (val < smallest) {
+      smallest = val;
+      smlIndex = i;
+    }
+
+    if (val > largest) {
+      largest = val;
+      lrgIndex = i;
+    }
+
+    maxDiff = Math.max(maxDiff, largest - smallest);
+
+    queue.push(val);
+  }
+
+  for (let i = 1; i <= arr.length - dev; i++) {
+    console.log('Once');
+    let newEndIndex = i + dev;
+    let newVal = arr[newEndIndex];
+
+    // maxDiff = difference(arr.slice(i, i + dev));
+
+    if (newVal > largest || lrgIndex < i) {
+      largest = newVal;
+      lrgIndex = newEndIndex;
+
+      //   if (!(i > smlIndex)) {
+      //     maxDiff = Math.max(maxDiff, largest - smallest);
+      //   } else {
+      //     for (let x = i; x < dev; x++) {
+      //       if (arr[x] < smallest) {
+      //         smallest = arr[x];
+      //         smlIndex = x;
+      //       }
+      //     }
+      //
+      maxDiff = difference(arr.slice(i, i + dev));
+    } else if (newVal < smallest || smlIndex < i) {
+      smallest = newVal;
+      smlIndex = newEndIndex;
+
+      // if (!(i > lrgIndex)) {
+      //   maxDiff = Math.max(maxDiff, largest - smallest);
+      // } else {
+      //   for (let x = i; x < dev; x++) {
+      //     if (arr[x] > largest) {
+      //       largest = arr[x];
+      //       lrgIndex = x;
+      //     }
+      //   }
+
+      maxDiff = difference(arr.slice(i, i + dev));
+    }
+
+    console.log('Highest: ' + largest + ' Smallest: ' + smallest);
+  }
+
+  return maxDiff;
+}
+
+console.log(deviationBetter([6, 9, 4, 7, 4, 1], 3));
+
+// var randArr = shuffledArr(100000);
+//
+// let start = performance();
+// console.log(deviation(randArr, 1000));
+// let end = performance();
+//
+// console.log(
+//   `Algo for deviation finished in ${((end - start) / 1000).toFixed(3)} seconds`
+// );
--- QueueStacks/queueFromStacks.js
+++ QueueStacks/queueFromStacks.js
@@ -1,54 +1,47 @@
 function Queue() {
-    let stack = [];
-    let stack2 = [];
+  let stack = [];
+  let stack2 = [];
 
-    function enqueue(elm) {
-		if (stack.length === 0) {
-			let length = stack2.length;
-			for (let i = 0; i < length; i++) {
-				stack.push(stack2.pop());
-			}
+  function enqueue(elm) {
+    if (stack.length === 0) {
+      while (stack2.length > 0) {
+        stack.push(stack2.pop());
+      }
+    }
 
-        }
+    stack.push(elm);
+  }
 
-        	stack.push(elm);
-	}
+  function dequeue(elm) {
+    if (stack2.length === 0) {
+      while (stack.length > 0) {
+        stack2.push(stack.pop());
+      }
+    }
 
-	function dequeue(elm) {
-		let length = stack.length;
-		if (stack2.length === 0) {
+    return stack2.pop();
+  }
 
-			for (let i = 0; i < length; i++) {
-				stack2.push(stack.pop());
-			}
-		}
+  function review() {
+    console.log(`Stack one is ${stack}`);
+    console.log(`Stack two is ${stack2}`);
+  }
 
-
-		return stack2.pop();
-	}
-
-	function review() {
-		console.log(`Stack one is ${stack}`);
-		console.log(`Stack two is ${stack2}`);
-	}
-
-    return {
-		enqueue,
-		dequeue,
-		review
-	}
-};
+  return {
+    enqueue,
+    dequeue,
+    review
+  };
+}
 
 const q = Queue();
 
-q.enqueue('1')
-q.enqueue('2')
-q.enqueue('3')
-q.enqueue('4')
-q.enqueue('5')
+q.enqueue('1');
+q.enqueue('2');
+q.enqueue('3');
+q.enqueue('4');
+q.enqueue('5');
 
 for (let i = 1; i <= 5; i++) {
-
-	console.log(q.dequeue());
-
+  console.log(q.dequeue());
 }
