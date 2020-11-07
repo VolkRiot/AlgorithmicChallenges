bdba6bfe63aa2ad6d92e6ebc6cc47d18dc2dd2aa
Mikhail Metrikin <metrikin@gmail.com>
2020-11-07 13:41:04 -0800

    Grokking Graphs

--- GrokkingAlgos/graphs.py
+++ GrokkingAlgos/graphs.py
@@ -13,16 +13,22 @@ graph["jonny"] = []
 search_queue = deque()
 search_queue += graph["you"]
 
+searched = []
+
 def breadth_first_search():
   current_search_queue = search_queue
   
   while current_search_queue:
     person = current_search_queue.popleft()
-    if person_is_seller(person):
-      print(f'{person} is a mango seller')
-      return True
-    else:
-      current_search_queue += graph[person]
+    if person not in searched:
+      if person_is_seller(person):
+        print(f'{person} is a mango seller')
+        return True
+      else:
+        current_search_queue += graph[person]
+        person.push(searched)
+
+    return False
 
 
 def person_is_seller(person):
