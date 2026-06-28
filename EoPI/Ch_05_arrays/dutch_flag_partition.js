7948172135fd8dbfbabfeeae55bb78d4b9d26525
Mikhail Metrikin <metrikin@gmail.com>
2026-06-28 14:43:45 -0700

    reset the EPI book folder

---
+++ EoPI/Ch_05_arrays/dutch_flag_partition.js
@@ -0,0 +1,6 @@
+function dutchPartition(arr, pivot) {}
+
+// console.log(dutchPartition([1, 1, 2, 0, 1, 2, 0, 1, 0, 2, 1], 2));
+// console.log(dutchPartitionPointers([0, 3, 5, 1, 2, 0, 5, 0, 2, 1, 1], 4));
+// console.log(dutchPartitionByValue([1, 1, 3, 1, 2, 3, 3, 2, 3, 2]));
+// console.log(dutchPartitionByValue([1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]));
